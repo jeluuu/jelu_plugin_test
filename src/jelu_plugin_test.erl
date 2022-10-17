f172ee0f6e33fbabcfcfbafb71eef3da7b9354d6
Abdul Jaleel <abduljaleelva@netstratum.com>
2022-10-17 16:21:36 +0530

    llll

--- src/jelu_plugin_test.erl
+++ src/jelu_plugin_test.erl
@@ -170,8 +170,8 @@ on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
 on_message_publish(Message = #message{payload = <<"netstratum">>}, _Env) ->
     lager:start(),
     io:format("~n -------- Welcome to Nestratum -------- ~n "),
-    % lager:error("welcome to nestratum123"),
-    lager:info("Welcome to Nestratum1"),
+    lager:error("welcome to nestratum123"),
+    % lager:info("Welcome to Nestratum1"),
     {ok, Message};
 
 on_message_publish(Message, _Env) ->
